c9e40a739792db822e1668a65726c7083fa0f396
Dave Jorgenson <jorgenson.dave@gmail.com>
2022-01-09 15:45:32 -0500

    initial assets

--- Assets/huntress/Huntress.cs
+++ Assets/huntress/Huntress.cs
@@ -4,27 +4,67 @@ using UnityEngine;
 
 public class Huntress : MonoBehaviour
 {
+
+    //left off video at 1:46 - section 11 obstacles
+
+    private int moveSpeed = 10;
+    //private Animation anim;
+    private bool crouching = false;
+    private bool jumping = false;
+    private float horizontalMove = 0f;
+
+    public CharacterController2D controller;
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //controller = gameObject.Get
+        //anim = gameObject.GetComponent<Animation>();
+        //anim["spin"].layer = 123;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // if (anim.isPlaying) {
+        //     return;
+        // }
+
+        horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+        if (Input.GetButtonDown("Jump")) {
+            jumping = true;
+            animator.SetBool("isJumping", true);
+        }
+
+        if (Input.GetButtonDown("Crouch")) {
+            crouching = true;
+        } 
+        else if (Input.GetButtonUp("Crouch")) {
+            crouching = false;
+        }
     }
 
-    private void OnMouseDown() {
-        GetComponent<SpriteRenderer>().color = Color.red;
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouching, jumping);
+        jumping = false;
     }
 
-    private void OnMouseUp() {
+    public void OnLanding() {
+        animator.SetBool("isJumping", false);
+        Debug.Log("landed");
+    }
+
+    private void OnMouseUp()
+    {
         GetComponent<SpriteRenderer>().color = Color.white;
     }
 
-    private void OnMouseDrag() {
+    private void OnMouseDrag()
+    {
         //transform.position = Input.MousePosition; //screen coordinates
 
         Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
